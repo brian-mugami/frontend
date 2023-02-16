b2fc0253bc6e2af105622d06e2a32926e4bd98fa
aguvasumugami@gmail.com <aguvasumugami@gmail.com>
2023-02-17 00:53:24 +0300

    cp kindred ui

--- src/pages/UserPages/Home.js
+++ src/pages/UserPages/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PageContent from "../../components/UserComponents/PageContent";
+import "./Home.css"
 
 
 function HomePage() {
@@ -7,6 +8,11 @@ function HomePage() {
     <React.Fragment>
     <PageContent title="Welcome!">
       <p>This is the Kindred Inventory App!!</p>
+      <div className="container">
+	      <div className="center">
+		      <h1 className="apple">KINDRED</h1>
+	      </div>
+      </div>
     </PageContent>
     </React.Fragment>
   );
