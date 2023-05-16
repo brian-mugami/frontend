1053151c87ec765fc38c1d75aede086ade14a5a5
Brian <brianmugz1@gmail.com>
2023-05-16 11:47:24 +0300

    payTypes

--- src/data/paymentTypes.js
+++ src/data/paymentTypes.js
@@ -26,24 +26,6 @@ export const currencyTypes = [
 ]
 
 export const unitTypes = [
-<<<<<<< HEAD
-    {id: 1, unit_type: "M"},
-    {id: 2, unit_type: "unit"},
-    {id: 3, unit_type: "L"},
-    {id: 4, unit_type: "mm"},
-    {id: 5, unit_type: "Kg"},
-    {id: 6, unit_type: "g"},
-    {id: 7, unit_type: "mg"},
-    {id: 9, unit_type: "cm"},
-    {id: 10, unit_type: "mg"},
-    {id: 11, unit_type: "ml"},
-    {id: 12, unit_type: "m3"},
-    {id: 14, unit_type: "cm2"},
-    {id: 1, unit_type: "inch"},
-    {id: 1, unit_type: "feet"},
-    {id: 13, unit_type: "m2"},
-    {id: 8, unit_type: "tonnes"},
-=======
     { id: 1,unit_type: "M"},
     { id: 2,unit_type: "cm"},
     { id: 3,unit_type: "feet"},
@@ -59,5 +41,4 @@ export const unitTypes = [
     { id: 13,unit_type: "m2"},
     { id: 14,unit_type: "cm2"},
 
->>>>>>> e8d5dbe3c87811de90cebde227c7452d5e3bf903
 ]
