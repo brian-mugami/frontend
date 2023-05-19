06b761532f5759964768e98b2a9e372acae3a4d8
Brian <brianmugz1@gmail.com>
2023-05-19 12:46:11 +0300

    misc issues

--- src/components/InventoryBalancesComponents/MiscIssueForm.js
+++ src/components/InventoryBalancesComponents/MiscIssueForm.js
@@ -1,11 +1,23 @@
 import React from "react";
 import { Form, json, redirect, useRouteLoaderData } from "react-router-dom";
 import { getAuthToken } from "../../util/Auth";
+import { useActionData } from "react-router-dom/dist/umd/react-router-dom.development";
 
 function MiscIssueForm() {
-  const { items } = useRouteLoaderData("inventory-balances");
+  const { items, adjAccounts } = useRouteLoaderData("inventory-balances");
+  const data = useActionData();
   return (
     <React.Fragment>
+      <p>
+        {data && data.errors && (
+          <ul>
+            {Object.values(data.errors).map((err) => (
+              <li key={err}>{err}</li>
+            ))}
+          </ul>
+        )}
+        {data && data.message && <p>{data.message}</p>}
+      </p>
       <Form className="row g-3 needs-validation" noValidate method="post">
         <div className="col-md-4 position-relative">
           <label htmlFor="validationTooltip01" className="form-label">
@@ -43,6 +55,24 @@ function MiscIssueForm() {
           />
           <div className="valid-tooltip">Looks good!</div>
         </div>
+        <div className="col-md-4 position-relative">
+          <label htmlFor="validationTooltip02" className="form-label">
+            Account
+          </label>
+          <select
+            name="account_name"
+            required
+            className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          >
+            {adjAccounts.map((account) => (
+              <option key={account.id} value={account.account_name}>
+                {" "}
+                {account.account_name}
+              </option>
+            ))}
+          </select>
+          <div className="valid-tooltip">Looks good!</div>
+        </div>
         <div className="col-12">
           <button className="btn btn-primary" type="submit">
             Submit form
@@ -55,28 +85,37 @@ function MiscIssueForm() {
 
 export default MiscIssueForm;
 
-export async function action({request, param}){
-    const token = getAuthToken()
-    const method = request.method
+export async function action({ request, param }) {
+  const token = getAuthToken();
+  const method = request.method;
 
-    const data = await request.formData()
+  const data = await request.formData();
 
-    const miscIssueData = {
-        item_name: data.get('item_name'),
-        quantity: data.get('quantity'),
-    }
+  const miscIssueData = {
+    item_name: data.get("item_name"),
+    quantity: data.get("quantity"),
+    account_name: data.get("account_name"),
+  };
 
-    const response = await fetch("/inventory-issue", {
-        method: method,
-        headers: {
-            "content-type": "application/json",
-            "Authorization": "Bearer " + token,
-        },
-        body: JSON.stringify(miscIssueData)
-    })
-
-    if(!response.ok){
-        throw json({message: "Error fetching"},{status: 500})
-    }
-    return redirect("/inventory-balance")
+  const response = await fetch("/inventory-issue", {
+    method: method,
+    headers: {
+      "content-type": "application/json",
+      Authorization: "Bearer " + token,
+    },
+    body: JSON.stringify(miscIssueData),
+  });
+  if (response.status === 400) {
+    return response;
+  }
+  if (response.status === 404) {
+    return response;
+  }
+  if (response.status === 500) {
+    return response;
+  }
+  if (!response.ok) {
+    throw json({ message: "Error fetching" }, { status: 500 });
+  }
+  return redirect("/inventory-balance");
 }
--- src/components/InventoryBalancesComponents/MiscReceiptForm.js
+++ src/components/InventoryBalancesComponents/MiscReceiptForm.js
@@ -1,11 +1,23 @@
 import React from "react";
 import { Form, json, redirect, useRouteLoaderData } from "react-router-dom";
 import { getAuthToken } from "../../util/Auth";
+import { useActionData } from "react-router-dom/dist/umd/react-router-dom.development";
 
 function MiscReceiptForm() {
-    const {items} = useRouteLoaderData("inventory-balances")
+  const { items, adjAccounts } = useRouteLoaderData("inventory-balances");
+  const data = useActionData();
   return (
     <React.Fragment>
+      <p>
+        {data && data.errors && (
+          <ul>
+            {Object.values(data.errors).map((err) => (
+              <li key={err}>{err}</li>
+            ))}
+          </ul>
+        )}
+        {data && data.message && <p>{data.message}</p>}
+      </p>
       <Form className="row g-3 needs-validation" noValidate method="post">
         <div className="col-md-4 position-relative">
           <label htmlFor="validationTooltip01" className="form-label">
@@ -22,10 +34,10 @@ function MiscReceiptForm() {
             required
           />
           <datalist id="options">
-          {items.map((item) => (
-            <option key={item.id} value={item.item_name} />
-          ))}
-        </datalist>
+            {items.map((item) => (
+              <option key={item.id} value={item.item_name} />
+            ))}
+          </datalist>
           <div className="valid-tooltip">Looks good!</div>
         </div>
         <div className="col-md-4 position-relative">
@@ -57,9 +69,27 @@ function MiscReceiptForm() {
               aria-describedby="validationTooltipUsernamePrepend"
               required
             />
-            <div className="invalid-tooltip">
-              Please choose a unit cost.
-            </div>
+            <div className="invalid-tooltip">Please choose a unit cost.</div>
+          </div>
+        </div>
+        <div className="col-md-4 position-relative">
+          <label htmlFor="validationTooltipUsername" className="form-label">
+            Account
+          </label>
+          <div className="input-group has-validation">
+            <select
+              name="account_name"
+              required
+              className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            >
+              {adjAccounts.map((account) => (
+                <option key={account.id} value={account.account_name}>
+                  {" "}
+                  {account.account_name}
+                </option>
+              ))}
+            </select>
+            <div className="invalid-tooltip">Please choose a unit cost.</div>
           </div>
         </div>
         <div className="col-12">
@@ -74,29 +104,41 @@ function MiscReceiptForm() {
 
 export default MiscReceiptForm;
 
-export async function action({request, param}){
-    const token = getAuthToken()
-    const method = request.method
+export async function action({ request, param }) {
+  const token = getAuthToken();
+  const method = request.method;
 
-    const data = await request.formData()
+  const data = await request.formData();
 
-    const miscReceiptData = {
-        item_name: data.get('item_name'),
-        quantity: data.get('quantity'),
-        unit_cost: data.get('unit_cost')
-    }
+  const miscReceiptData = {
+    item_name: data.get("item_name"),
+    quantity: data.get("quantity"),
+    unit_cost: data.get("unit_cost"),
+    account_name: data.get("account_name"),
+  };
 
-    const response = await fetch("/inventory-balances", {
-        method: method,
-        headers: {
-            "content-type": "application/json",
-            "Authorization": "Bearer " + token,
-        },
-        body: JSON.stringify(miscReceiptData)
-    })
-
-    if(!response.ok){
-        throw json({message: "Error fetching"},{status: 500})
-    }
-    return redirect("/inventory-balance")
+  const response = await fetch("/inventory-balances", {
+    method: method,
+    headers: {
+      "content-type": "application/json",
+      Authorization: "Bearer " + token,
+    },
+    body: JSON.stringify(miscReceiptData),
+  });
+  if (response.status === 400) {
+    return response;
+  }
+  if (response.status === 404) {
+    return response;
+  }
+  if (response.status === 500) {
+    return response;
+  }
+  if (!response.ok) {
+    throw json(
+      { message: "An error occurred!Please try again" },
+      { status: 500 }
+    );
+  }
+  return redirect("/inventory-balance");
 }
