4c17573c3125c3ee12d8b60677286cf3aa3d3b4e
lusenakalee <lusenakalee@gmail.com>
2023-05-25 13:02:13 +0300

    looks only

--- src/components/InvoiceComponents/InvoiceItem.js
+++ src/components/InvoiceComponents/InvoiceItem.js
@@ -4,6 +4,25 @@ import {
   useActionData,
   useNavigate,
 } from "react-router-dom/dist/umd/react-router-dom.development";
+import { Fragment } from 'react'
+import {
+  BriefcaseIcon,
+  CalendarIcon,
+  CheckIcon,
+  ChevronDownIcon,
+  CurrencyDollarIcon,
+  LinkIcon,
+  MapPinIcon,
+  PencilIcon,
+} from '@heroicons/react/20/solid'
+import { Menu, Transition } from '@headlessui/react'
+
+import { List, ListItem, Title } from "@tremor/react";
+
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
 
 function InvoiceItem({ invoice }) {
   const token = useRouteLoaderData("root");
@@ -24,29 +43,148 @@ function InvoiceItem({ invoice }) {
 
   return (
     <React.Fragment>
-      {data && data.errors && (
+
+{data && data.errors && (
         <ul>
           {Object.values(data.errors).map((err) => (
             <li key={err}>{err}</li>
           ))}
         </ul>
       )}
-      {data && data.message && <p>{data.message}</p>}
-      <h1>{invoice.invoice_number}</h1>
-      <time>Date : {invoice.date}</time>
-      <h1>{invoice.accounted}</h1>
-      <h1>{invoice.amount}</h1>
-      <h1>{invoice.destination_type}</h1>
+
+{data && data.message && <p>{data.message}</p>}
+
+
+
+<div className="lg:flex lg:items-center lg:justify-between">
+      <div className="min-w-0 flex-1">
+        <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
+        {invoice.invoice_number}
+        </h2>
+        <div className="mt-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-6">
+          <div className="mt-2 flex items-center text-sm text-gray-500">
+            <BriefcaseIcon className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
+            {invoice.accounted}
+          </div>
+          <div className="mt-2 flex items-center text-sm text-gray-500">
+            <MapPinIcon className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
+            {invoice.destination_type}
+          </div>
+          <div className="mt-2 flex items-center text-sm text-gray-500">
+            <CurrencyDollarIcon className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
+            {invoice.amount}
+          </div>
+          <div className="mt-2 flex items-center text-sm text-gray-500">
+            <CalendarIcon className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
+            {invoice.date}          </div>
+        </div>
+      </div>
+
+      
+      <div className="mt-5 flex lg:ml-4 lg:mt-0">
+        <span className="hidden sm:block">
+          <Link to="edit">
+          <button
+            type="button"
+            className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+          >
+            <PencilIcon className="-ml-0.5 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
+            Edit
+          </button></Link>
+        </span>
+
+        <span className="ml-3 hidden sm:block">
+          <Link to="void">
+          <button
+            type="button"
+            className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+          >
+            <LinkIcon className="-ml-0.5 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
+           Void
+          </button>
+          </Link>
+        </span>
+
+        <span className="sm:ml-3">
+          <button
+            type="button"
+            onClick={startDeleteHandler}
+
+            className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            <CheckIcon className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden="true" />
+            Delete
+          </button>
+        </span>
+
+        {/* Dropdown */}
+        <Menu as="div" className="relative ml-3 sm:hidden">
+          <Menu.Button className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:ring-gray-400">
+            More
+            <ChevronDownIcon className="-mr-1 ml-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
+          </Menu.Button>
+
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-200"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items className="absolute right-0 z-10 -mr-1 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+              <Menu.Item>
+                {({ active }) => (
+                  <Link to="edit"
+                    className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                  >
+                    Edit
+                  </Link>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <Link to="void"
+                    className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                  >
+                    Void
+                    </Link>
+                )}
+              </Menu.Item>
+            </Menu.Items>
+          </Transition>
+        </Menu>
+      </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
       <p className="pt-2">
         {" "}
         <b>Items in invoice</b>
       </p>
+      <List>
       {invoice.purchase_items.map((item) => (
-        <h1>
+        <ListItem>
           {item.item.item_name} selling price : {item.item.price} buying price :{" "}
           {item.buying_price} Quantity ordered : {item.item_quantity}{" "}
-        </h1>
-      ))}
+          </ListItem>      ))}
+      </List>
       <h1>{invoice.purchase_type}</h1>
       <h1>Supplier : {invoice.supplier.supplier_name}</h1>
       <p className="pt-2">
@@ -61,8 +199,9 @@ function InvoiceItem({ invoice }) {
       ))}
 
       <b>Payments</b>
+      <List>
       {invoice.payments.map((payment) => (
-        <div>
+        <ListItem>
           <h1>{payment.payment_status}</h1>
           <h1>
             {" "}
@@ -70,42 +209,20 @@ function InvoiceItem({ invoice }) {
             {payment.approval_status}
           </h1>
           <h1> Transaction code : {payment.payment_description}</h1>
-        </div>
-      ))}
+        </ListItem>
+      ))}</List>
 
       <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start"></div>
       {token && (
         <menu>
           <div className="flex">
-            <div className="pr-5">
-              <Link
-                to="edit"
-                className="rounded-md bg-blue-300  px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
-              >
-                Edit invoice
-              </Link>
-            </div>
-            <div className="pr-5">
-              <Link
-                to="void"
-                className="rounded-md bg-blue-300  px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
-              >
-                Void invoice
-              </Link>
-            </div>
+           
             <div className="pr-5">
               <button className="btn btn-warning" onClick={cancelHandler}>
                 Back
               </button>
             </div>
-            <div>
-              <button
-                onClick={startDeleteHandler}
-                className="text-sm font-semibold leading-6 text-black"
-              >
-                Delete <span aria-hidden="true">x</span>
-              </button>
-            </div>
+         
           </div>
         </menu>
       )}
--- src/components/ReceiptComponents/ReceiptItem.js
+++ src/components/ReceiptComponents/ReceiptItem.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { Link, useRouteLoaderData, useSubmit } from "react-router-dom";
+import { useNavigate } from "react-router-dom/dist/umd/react-router-dom.development";
+import { getAuthToken } from "../../util/Auth";
+import { Card, Title, Text, Flex, Button } from "@tremor/react";
+import {
 
-function ReceiptItem({ receipt }) {
+  PencilIcon,
+} from '@heroicons/react/20/solid'
+
+function ReceiptItem({ receipt, id }) {
   const token = useRouteLoaderData("root");
   const submit = useSubmit();
+  const navigate = useNavigate();
 
   function startDeleteHandler() {
     const proceed = window.confirm("Are you sure?");
@@ -13,21 +21,70 @@ function ReceiptItem({ receipt }) {
     }
   }
 
+  async function printReceiptHandler() {
+    const token = getAuthToken();
+
+    try {
+      const response = await fetch(`/receipt/download/test/${id}`, {
+        method: "GET",
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      });
+
+      if (response.ok) {
+        const blob = await response.blob();
+        const link = document.createElement("a");
+        link.href = URL.createObjectURL(blob);
+        link.download = "receipt " + receipt.receipt_number;
+        link.click();
+      } else {
+        console.error("Failed to download receipt PDF");
+      }
+    } catch (error) {
+      console.error("An error occurred", error);
+    }
+  }
+
+  function cancelHandler() {
+    navigate("..");
+  }
+
   return (
     <React.Fragment>
+      <Button size="sm" variant="secondary" onClick={cancelHandler}>
+        Back
+      </Button>
+
       <h1>{receipt.receipt_number}</h1>
       <time>Date : {receipt.date}</time>
       {token && (
         <menu>
-          <Link to="edit">
-            <button className="btn btn-success">Edit</button>
-          </Link>
-          <button onClick={startDeleteHandler} className="btn btn-danger">
-            Delete Receipt
+          <Flex justifyContent="end" className="space-x-2">
+            <Link to="edit">
+            <button
+            type="button"
+            className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-4 ring-inset ring-gray-700 hover:bg-gray-50"
+          >
+            <PencilIcon className="-ml-0.5 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
+            Edit
           </button>
-          <Link to="void">
-            <button className="btn btn-primary">Void this receipt</button>
-          </Link>
+            </Link>
+
+            <Button onClick={startDeleteHandler} size="sm" variant="primary">
+              Delete
+            </Button>
+
+            <Link to="void">
+              <Button size="sm" variant="primary">
+                Void
+              </Button>
+            </Link>
+
+            <Button onClick={printReceiptHandler} size="sm" variant="primary">
+              Print
+            </Button>
+          </Flex>
         </menu>
       )}
     </React.Fragment>
